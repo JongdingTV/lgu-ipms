f88de55276970fc7f778be188ff9c632c7e9bf08
jongding <151025211+JongdingTV@users.noreply.github.com>
2026-01-07 22:02:14 +0800

    Update budget_resources.php

--- budget-resources/budget_resources.php
+++ budget-resources/budget_resources.php
@@ -38,7 +38,6 @@ $conn->close();
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../assets/style.css" />
-    <link rel="stylesheet" href="budget-resources.css" />
 </head>
 <body>
     <header class="nav" id="navbar">
