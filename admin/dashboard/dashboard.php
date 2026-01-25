2e169bc3b9e12f97b9a7a5e6235009fc83ee139a
jongding <151025211+JongdingTV@users.noreply.github.com>
2026-01-25 13:34:17 +0800

    Update dashboard.php

--- admin/dashboard/dashboard.php
+++ admin/dashboard/dashboard.php
@@ -38,12 +38,12 @@ $db->close();
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <?php echo get_app_config_script(); ?>
-    <script src="/security-no-back.js?v=<?php echo time(); ?>"></script>
+    <script src="../../security-no-back.js?v=<?php echo time(); ?>"></script>
 </head>
 <body>
     <header class="nav" id="navbar">
         <div class="nav-logo">
-            <img src="../../logocityhall.png" alt="City Hall Logo" class="logo-img">
+            <img src="/logocityhall.png" alt="City Hall Logo" class="logo-img">
             <span class="logo-text">IPMS</span>
         </div>
         <div class="nav-links">
