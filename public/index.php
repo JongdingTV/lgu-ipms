8bd51ca5b41dc7ec6a3b4f4bbd966ae164f5f772
jongding <151025211+JongdingTV@users.noreply.github.com>
2026-01-26 21:04:00 +0800

    Update index.php

--- public/index.php
+++ public/index.php
@@ -557,7 +557,7 @@ require_once INCLUDES_PATH . '/security.php';
 	<nav class="navbar navbar-expand-lg sticky-top">
 		<div class="container-fluid">
 			<a class="navbar-brand" href="/">
-				<img src="<?php echo ASSETS_URL; ?>/images/logocityhall.png" alt="City Hall Logo" class="logo-img">
+				<img src="<?php echo ASSETS_URL; ?>/logocityhall.png" alt="City Hall Logo" class="logo-img">
 				<span class="brand-text">LGU IPMS</span>
 			</a>
 			<button class="navbar-toggler hamburger-menu" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
