7e0e6f08bee8ca121bf62116d4e3a448bca6077b
Ojison <magrimbao9@gmail.com>
2026-01-27 22:37:50 +0800

    fixing user login css 2

--- user-dashboard/user-login.php
+++ user-dashboard/user-login.php
@@ -34,7 +34,7 @@ require_once dirname(__DIR__) . '/config/email.php';
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>LGU | Login</title>
-<link rel="icon" type="image/png" href="/logocityhall.png">
+<link rel="icon" type="image/png" href="/assets/logocityhall.png">
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
