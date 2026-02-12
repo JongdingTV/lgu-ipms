c3b854721e78a4707f03d7155381ff99a285ce4d
jongding <151025211+JongdingTV@users.noreply.github.com>
2026-02-12 11:22:49 +0800

    fixed blur

--- admin/settings.php
+++ admin/settings.php
@@ -161,7 +161,7 @@ if (isset($db) && !$db->connect_error) {
         </div>
         <div class="nav-divider"></div>
         <div class="ac-723b1a7b">
-            <a href="#" id="logoutBtn" class="ac-bb30b003">>
+            <a href="#" id="logoutBtn" class="ac-bb30b003">
                 <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
                     <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path>
                     <polyline points="16 17 21 12 16 7"></polyline>
